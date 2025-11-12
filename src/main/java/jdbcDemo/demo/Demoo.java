bea2a7e6f59424327a26193b8c3a007c3e736cb6
subhrajit-github <subudhisubhrajit@gmail.com>
2025-11-12 20:59:28 +0530

    commit

--- src/main/java/jdbcDemo/demo/Demoo.java
+++ src/main/java/jdbcDemo/demo/Demoo.java
@@ -1,12 +1,27 @@
 package jdbcDemo.demo;
 
-public class Demoo {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Demoo {
+ 
+	private static String url="jdbc:postgresql://localhost:5432/school";
+	private static String user="postgres";
+	private static String password="123";
 	public static void main(String[] args) {
 		try {
+			//Step 1 load and register driver software
 			Class.forName("org.postgresql.Driver");
-			System.out.println("driver loaded!!!");
-		} catch (ClassNotFoundException e) {
+			System.out.println("driver loaded!!!");	
+			
+			//Step 2 establishing connection to DB
+			Connection con=DriverManager.getConnection(url, user, password);
+			System.out.println("connection established to school DB");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
